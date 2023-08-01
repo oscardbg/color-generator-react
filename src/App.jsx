3f025b956fe4db61f8137c566b0d08de39dd9985
Vieniamin <vieniamin@mail.com>
2023-08-01 17:42:49 -0500

    Form styles and default colors shown

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import Values from "values.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const defaultColor = "#ffa500";
+  const defaultValues = new Values(defaultColor).all(10);
+
+  const [color, setColor] = useState(defaultColor);
+  const [amount, setAmount] = useState(10);
+  const [list, setList] = useState(defaultValues);
+
+  return (
+    <div className="container">
+      <h1>Tints and Shades</h1>
+      <Form color={color} setColor={setColor} amount={amount} setAmount={setAmount} />
+      <ColorList list={list} />
+    </div>
+  );
+}
+
+function Form({ color, setColor, amount, setAmount }) {
+  return (
+    <>
+      <form action="">
+        <div>
+          <label htmlFor="color">Pick a color:</label>
+          <input type="color" name="color" value={color} onChange={(e) => setColor(e.target.value)} />
+          <input type="text" name="color" value={color} onChange={(e) => setColor(e.target.value)} />
+          <input
+            type="range"
+            name="amount"
+            min={0}
+            max={100}
+            step={5}
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <label htmlFor="amount">{amount} colors</label>
+        </div>
+        <button type="submit">Generate</button>
+      </form>
+    </>
+  );
+}
+
+function ColorList({ list }) {
+  return (
+    <>
+      <ul className="color-list">
+        {list.map((color, i) => (
+          <Color key={i} color={color} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function Color({ color }) {
+  const { rgb, type, weight } = color;
+
+  let rgbColor = rgb.toString();
+
+  let boxStyles = { backgroundColor: `rgb(${rgbColor})` };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <li>
+        <div className="box" style={boxStyles}></div>
+        <div className="info">
+          <p>{rgbColor} </p>
+          <p>{type}</p>
+        </div>
+      </li>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
