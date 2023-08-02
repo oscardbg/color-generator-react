837cfe513da7f27a397a20ca31595c63590be829
Vieniamin <vieniamin@mail.com>
2023-08-01 19:06:57 -0500

    Colors generated with form fully working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import Values from "values.js";
 
+import rgbToHex from "./utils";
+
 function App() {
   const defaultColor = "#ffa500";
   const defaultValues = new Values(defaultColor).all(10);
@@ -9,19 +11,38 @@ function App() {
   const [amount, setAmount] = useState(10);
   const [list, setList] = useState(defaultValues);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      let newList = new Values(color).all(parseInt(amount));
+      setList(newList);
+      console.log(amount);
+      console.log(newList);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="container">
       <h1>Tints and Shades</h1>
-      <Form color={color} setColor={setColor} amount={amount} setAmount={setAmount} />
+      <Form
+        color={color}
+        setColor={setColor}
+        amount={amount}
+        setAmount={setAmount}
+        handleSubmit={handleSubmit}
+      />
       <ColorList list={list} />
     </div>
   );
 }
 
-function Form({ color, setColor, amount, setAmount }) {
+function Form({ color, setColor, amount, setAmount, handleSubmit }) {
   return (
     <>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="color">Pick a color:</label>
           <input type="color" name="color" value={color} onChange={(e) => setColor(e.target.value)} />
@@ -29,13 +50,13 @@ function Form({ color, setColor, amount, setAmount }) {
           <input
             type="range"
             name="amount"
-            min={0}
-            max={100}
-            step={5}
+            min={1}
+            max={50}
+            // step={5}
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
           />
-          <label htmlFor="amount">{amount} colors</label>
+          <label htmlFor="amount">{amount}% variation</label>
         </div>
         <button type="submit">Generate</button>
       </form>
@@ -59,16 +80,19 @@ function Color({ color }) {
   const { rgb, type, weight } = color;
 
   let rgbColor = rgb.toString();
+  let hexColor = rgbToHex(...rgb);
 
   let boxStyles = { backgroundColor: `rgb(${rgbColor})` };
 
   return (
     <>
       <li>
-        <div className="box" style={boxStyles}></div>
+        <div className="box" style={boxStyles}>
+          <span>{weight}%</span>
+        </div>
         <div className="info">
-          <p>{rgbColor} </p>
-          <p>{type}</p>
+          <p>rgb({rgbColor})</p>
+          <p>{hexColor} </p>
         </div>
       </li>
     </>
